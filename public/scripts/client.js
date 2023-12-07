5726e54930cc1f2efcc43694e06b85c9f55c3384
Kershia1 <insatiable.meerkat@gmail.com>
2023-12-07 09:33:24 -0800

    Beginning creationg of load tweets funtion.  Still debugging reference error : timeago not defined.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,18 +5,12 @@
  */
 
 // Fake data taken from initial-tweets.json
+console.log("in client.js");
 $(document).ready(function () {
-  // this function is called when the page loads
-  //it wraps everything in the document ready function
-  //then parse all the data and any scripts will be encountered to prevent errors when loading the page, until DOM is ready to be manipulated
-
-
-  /*remeber is shorthand for 
-  document.addEventListener('DOMContentLoaded', function() {
-  // Your code goes here
-});
-  */
   // this function is called when the user submits a tweet
+
+  //this is the pre-made data that I'm using to test the app
+/*  console.log("in document ready");
 const data = [
   {
     "user": {
@@ -43,17 +37,32 @@ const data = [
   }
 ];
 
-
-
 const renderTweets = function (tweets) {
+  console.log("in renderTweets");
   $('#tweets-container').empty();// empties the tweets container, I can't remember why I need to do this..? 
   for (let tweet of tweets) {// loops through tweets
     let $tweet = createTweetElement(tweet);// creates a variable that calls the createTweetElement function and passes in the tweet object
     $('#tweets-container').append($tweet);// takes return value and appends it to the tweets container
   }
 };
+*/
+
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
 
 const createTweetElement = function (tweet) {
+  let timeagoMoment = timeago();
+  console.log("in createTweetElement");
   let $tweet = $(`
 <article class="tweets">
         <header class="tweets-header">
@@ -74,7 +83,7 @@ const createTweetElement = function (tweet) {
         </div>
           <footer class="tweets-footer">
             <div class="tweets-time">
-              <p>${timeago.format(tweet.created_at)}</p>
+              <p>${timeagoMoment.format(tweet.created_at)}</p>
             </div>
             <div class="tweets-icons">
               <i class="fa-solid fa-flag"></i>
@@ -86,9 +95,33 @@ const createTweetElement = function (tweet) {
   return $tweet;
 };
 
-renderTweets(data);
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+/*renderTweets(data);
+
+
+//using AJAX with jQuery
+//Serialize the form data
+//Use the jQuery library to submit a POST request that sends the serialized data to the server
+//The jQuery .serialize() function turns a set of form data into a query string. This serialized data should be sent to the server in the data field of the AJAX POST request.
+
+  const loadTweets = function () {
+//add an event listener that listens for the submit event
+//prevent the default behaviour of the submit event (data submission and page refresh)
+event.preventDefault();
+//create an AJAX POST request in client.js that sends the form data to the server.
+  } */
 
 });
+
+
+
+
 
 /**
  * its been a while since I've done this, so I'm going to try to write out the steps I need to take to get this working
@@ -109,5 +142,13 @@ renderTweets(data);
  * nervous about the timeago function, but I think I can figure it out
  * 
  * 
- * 
- */
+ *   // this function is called when the page loads
+  //it wraps everything in the document ready function
+  //then parse all the data and any scripts will be encountered to prevent errors when loading the page, until DOM is ready to be manipulated
+
+
+  /*remeber is shorthand for 
+  document.addEventListener('DOMContentLoaded', function() {
+  // Your code goes here
+});
+  */
