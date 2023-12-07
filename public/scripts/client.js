cf419b86ab46fe455f49ef739e88ffc83df00980
Kershia1 <insatiable.meerkat@gmail.com>
2023-12-06 23:14:02 -0800

    created render tweets in client js.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,18 @@
  */
 
 // Fake data taken from initial-tweets.json
+$(document).ready(function () {
+  // this function is called when the page loads
+  //it wraps everything in the document ready function
+  //then parse all the data and any scripts will be encountered to prevent errors when loading the page, until DOM is ready to be manipulated
+
+
+  /*remeber is shorthand for 
+  document.addEventListener('DOMContentLoaded', function() {
+  // Your code goes here
+});
+  */
+  // this function is called when the user submits a tweet
 const data = [
   {
     "user": {
@@ -22,26 +34,31 @@ const data = [
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
   }
-]
+];
 
-const renderTweets = function(tweets) {
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-}
 
-const createTweetElement = function(tweet) {
-let $tweet = $(`
+
+const renderTweets = function (tweets) {
+  $('#tweets-container').empty();// empties the tweets container, I can't remember why I need to do this..? 
+  for (let tweet of tweets) {// loops through tweets
+    let $tweet = createTweetElement(tweet);// creates a variable that calls the createTweetElement function and passes in the tweet object
+    $('#tweets-container').append($tweet);// takes return value and appends it to the tweets container
+  }
+};
+
+const createTweetElement = function (tweet) {
+  let $tweet = $(`
 <article class="tweets">
         <header class="tweets-header">
             <div class="user-img">
-              <img src="${tweet.user.img}" alt="Profile picture of ${tweet.user.name}"> 
+              <img src="${tweet.user.avatars}" alt="Profile picture of ${tweet.user.name}"> 
             </div> 
                 <div class="user-name">
                 <h6>${tweet.user.name}</h6>
@@ -57,7 +74,7 @@ let $tweet = $(`
         </div>
           <footer class="tweets-footer">
             <div class="tweets-time">
-              <p>${timeago.format(tweetData.created_at)}</p>
+              <p>${timeago.format(tweet.created_at)}</p>
             </div>
             <div class="tweets-icons">
               <i class="fa-solid fa-flag"></i>
@@ -66,10 +83,12 @@ let $tweet = $(`
             </div>
           </footer>
         </article> `);
-return $tweet;
+  return $tweet;
 };
 
 renderTweets(data);
+
+});
 
 /**
  * its been a while since I've done this, so I'm going to try to write out the steps I need to take to get this working
@@ -88,5 +107,7 @@ renderTweets(data);
  * and returns the date in a string
  * 
  * nervous about the timeago function, but I think I can figure it out
+ * 
+ * 
  * 
  */
