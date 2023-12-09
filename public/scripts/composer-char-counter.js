9b5841c3578c13b56c099cd19af49dcef5035160
Kershia1 <insatiable.meerkat@gmail.com>
2023-12-09 07:40:49 -0800

    removed hard coded tweets for easier readability in index.html.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 
 // This function counts the characters in the tweet and updates the counter
 function count() {
-  const chars = $("#tweet-text").val().length;
+  const chars = $("#tweets-text").val().length;
   $(".counter").text(140 - chars);
   if (chars > 140) {
     $(".counter").addClass("negativetweet");
