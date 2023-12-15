4b19db43e5ac48bed7077f32d0143d316dd194b8
Kershia1 <insatiable.meerkat@gmail.com>
2023-12-14 22:39:48 -0800

    header invisible, next stylign the .right dic for the body.

---
+++ public/scripts/responsive-design-changes.js
@@ -0,0 +1,12 @@
+// responsive.js
+// document.addEventListener('DOMContentLoaded', function() {
+//   window.addEventListener('resize', function() {
+//     if (window.innerWidth <= 768) { // adjust the value as per your tablet size
+//       let userIcon = document.querySelector('.user-icon');
+//       let userName = document.querySelector('.user-name');
+//       let nav = document.querySelector('nav');
+//       nav.appendChild(userIcon);
+//       nav.appendChild(userName);
+//     }
+//   });
+// });
